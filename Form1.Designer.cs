485b0b5497ca1e8d934580bc8050207df1b39082
Scott Kellman <scottkellman@hotmail.com>
2020-07-08 15:00:25 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace QPReadWriteRegistry
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxKeyName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxKeyValue = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonWriteKey = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxReadKeyValue = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxReadKeyName = new System.Windows.Forms.TextBox();
+            this.ButtonReadKey = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxKeyName
+            // 
+            this.textBoxKeyName.Location = new System.Drawing.Point(121, 40);
+            this.textBoxKeyName.Name = "textBoxKeyName";
+            this.textBoxKeyName.Size = new System.Drawing.Size(481, 20);
+            this.textBoxKeyName.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Registry Key Name";
+            // 
+            // textBoxKeyValue
+            // 
+            this.textBoxKeyValue.Location = new System.Drawing.Point(121, 76);
+            this.textBoxKeyValue.Name = "textBoxKeyValue";
+            this.textBoxKeyValue.Size = new System.Drawing.Size(112, 20);
+            this.textBoxKeyValue.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Registry Key Value";
+            // 
+            // buttonWriteKey
+            // 
+            this.buttonWriteKey.Location = new System.Drawing.Point(121, 112);
+            this.buttonWriteKey.Name = "buttonWriteKey";
+            this.buttonWriteKey.Size = new System.Drawing.Size(75, 23);
+            this.buttonWriteKey.TabIndex = 4;
+            this.buttonWriteKey.Text = "Write Key";
+            this.buttonWriteKey.UseVisualStyleBackColor = true;
+            this.buttonWriteKey.Click += new System.EventHandler(this.buttonWriteKey_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 205);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Registry Key Value";
+            // 
+            // textBoxReadKeyValue
+            // 
+            this.textBoxReadKeyValue.Location = new System.Drawing.Point(121, 202);
+            this.textBoxReadKeyValue.Name = "textBoxReadKeyValue";
+            this.textBoxReadKeyValue.Size = new System.Drawing.Size(112, 20);
+            this.textBoxReadKeyValue.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 169);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Registry Key Name";
+            // 
+            // textBoxReadKeyName
+            // 
+            this.textBoxReadKeyName.Location = new System.Drawing.Point(121, 166);
+            this.textBoxReadKeyName.Name = "textBoxReadKeyName";
+            this.textBoxReadKeyName.Size = new System.Drawing.Size(481, 20);
+            this.textBoxReadKeyName.TabIndex = 5;
+            // 
+            // ButtonReadKey
+            // 
+            this.ButtonReadKey.Location = new System.Drawing.Point(121, 240);
+            this.ButtonReadKey.Name = "ButtonReadKey";
+            this.ButtonReadKey.Size = new System.Drawing.Size(75, 23);
+            this.ButtonReadKey.TabIndex = 9;
+            this.ButtonReadKey.Text = "Read Key";
+            this.ButtonReadKey.UseVisualStyleBackColor = true;
+            this.ButtonReadKey.Click += new System.EventHandler(this.ButtonReadKey_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ButtonReadKey);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxReadKeyValue);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxReadKeyName);
+            this.Controls.Add(this.buttonWriteKey);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxKeyValue);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxKeyName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxKeyName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxKeyValue;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonWriteKey;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxReadKeyValue;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxReadKeyName;
+        private System.Windows.Forms.Button ButtonReadKey;
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QPReadWriteRegistry
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+        private const string SQLKEY = @"DT3\SQL_Installed";
+        private const string DACKEY = @"DT3\DAC_Installed";
+        private const string FLUMEKEY = @"DT3\Flume_Installed";
+        private const string SEVENZIPKEY = @"DT3\S7Zip_Installed";
+        private const string DOTNETKEY = @"DT3\DotNet_Installed";
+
+        private const string SQLKEYNAME = "sql_installed";
+        private const string DACKEYNAME = "dac_installed";
+        private const string FLUMEKEYNAME = "flume_installed";
+        private const string SEVENZIPKEYNAME = "7zip_installed";
+        private const string DOTNETKEYNAME = "dotnet_installed";
+
+        public static void WriteKeys()
+        {
+            //accessing the LocalMachine root element  
+            RegistryKey keySQL = Registry.CurrentUser.CreateSubKey(SQLKEY);
+            RegistryKey keyDAC = Registry.CurrentUser.CreateSubKey(DACKEY);
+            RegistryKey keyFlume = Registry.CurrentUser.CreateSubKey(FLUMEKEY);
+            RegistryKey key7Zip = Registry.CurrentUser.CreateSubKey(SEVENZIPKEY);
+            RegistryKey keyDotNet = Registry.CurrentUser.CreateSubKey(DOTNETKEY);
+
+
+            keySQL.SetValue(SQLKEYNAME, 0);
+            keyDAC.SetValue(DACKEYNAME, 0);
+            keyFlume.SetValue(FLUMEKEYNAME, 0);
+            key7Zip.SetValue(SEVENZIPKEYNAME, 0);
+            keyDotNet.SetValue(DOTNETKEYNAME, 0);
+        }
+
+        public static void ReadKeys()
+        {
+            //opening the subkey  
+            RegistryKey key1 = Registry.CurrentUser.OpenSubKey(SQLKEY);
+            RegistryKey key2 = Registry.CurrentUser.OpenSubKey(DACKEY);
+            RegistryKey key3 = Registry.CurrentUser.OpenSubKey(FLUMEKEY);
+            RegistryKey key4 = Registry.CurrentUser.OpenSubKey(SEVENZIPKEY);
+            RegistryKey key5 = Registry.CurrentUser.OpenSubKey(DOTNETKEY);
+
+            //if it does exist, retrieve the stored values  
+            if (key1 != null)
+            {
+                MessageBox.Show(key1.GetValue(SQLKEYNAME).ToString());
+                key1.Close();
+            }
+            //if it does exist, retrieve the stored values  
+            if (key2 != null)
+            {
+                MessageBox.Show(key2.GetValue(DACKEYNAME).ToString());
+                key2.Close();
+            }
+            //if it does exist, retrieve the stored values  
+            if (key3 != null)
+            {
+                MessageBox.Show(key3.GetValue(FLUMEKEYNAME).ToString());
+                key3.Close();
+            }
+            //if it does exist, retrieve the stored values  
+            if (key4 != null)
+            {
+                MessageBox.Show(key4.GetValue(SEVENZIPKEYNAME).ToString());
+                key4.Close();
+            }
+            //if it does exist, retrieve the stored values  
+            if (key5 != null)
+            {
+                MessageBox.Show(key5.GetValue(DOTNETKEYNAME).ToString());
+                key5.Close();
+            }
+        }
+    }
+}
